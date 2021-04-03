ace13d087a69f868e9b36e72127b7a0579af5c17
Andrea Storace <andrea.storace.uni@gmail.com>
2021-04-03 15:20:34 +0200

    deploy

---
+++ precache-manifest.1fcbcd4bc25210669749d7cfe8ac0805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51710f96882a84111301",
+    "url": "/tiresia/css/app.67cfb25c.css"
+  },
+  {
+    "revision": "234059e131d95615b2c1e8fb5de4372c",
+    "url": "/tiresia/index.html"
+  },
+  {
+    "revision": "51710f96882a84111301",
+    "url": "/tiresia/js/app.aba7eccf.js"
+  },
+  {
+    "revision": "8b9178fad35f00b66e2f",
+    "url": "/tiresia/js/chunk-vendors.5409e49f.js"
+  },
+  {
+    "revision": "1d56b75d5b423ffcb5707f7ea4a4281f",
+    "url": "/tiresia/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tiresia/robots.txt"
+  }
+]);
